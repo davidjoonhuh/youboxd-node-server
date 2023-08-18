2f8a7d5aa466488fc5f3ff63a807a3ed85602d50
davidjoonhuh <davidjo.huh@gmail.com>
2023-08-18 18:47:42 -0400

    New APIs- video barebones done, comments not yet

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import UserController from "./users/users-controller.js";
 import AuthController from './users/auth-controller.js';
+import VideosController from './videos/videos-controller.js';
+import CommentsController from './comments/comments-controller.js';
 import session from "express-session";
 import "dotenv/config";
 
@@ -35,4 +37,6 @@ if (process.env.NODE_ENV !== "development") {
 app.use(express.json());
 AuthController(app);
 UserController(app);
+CommentsController(app);
+VideosController(app);
 app.listen(process.env.PORT || 4000);
---
+++ comments/comments-controller.js
@@ -0,0 +1,43 @@
+import * as commentsDao from "./comments-dao.js";
+
+const CommentsController = (app) => {
+    app.delete('/api/comments/:id', deleteComment);
+    app.post('/api/comments', createComment);
+    app.get('/api/comments', findAllComments);
+    app.get('/api/comments/:id', findCommentById);
+    app.get('/api/authorcomments/:id', findCommentsByAuthorId);
+    app.get('/api/videocomments/:id', findCommentsByVideoId);
+}
+
+const deleteComment = async(req, res) => {
+    const id = req.params.id;
+    const status = await commentsDao.deleteCommentById(id);
+    res.json(status)
+}
+
+const createComment = async(req, res) => {
+    const newComment = await commentsDao.createComment(req.body);
+    res.json(newComment)
+}
+
+const findAllComments = async(req, res) => {
+    const comments = await commentsDao.findAllComments();
+    res.json(comments)
+}
+
+const findCommentById = async(req, res) => {
+    const id = req.params.id;
+    const video = await commentsDao.findCommentById(id);
+}
+
+const findCommentsByAuthorId = async(req, res) => {
+    const id = req.params.id;
+    const comments = await commentsDao.findAllCommentsByVideoId(id);
+}
+
+const findCommentsByVideoId = async(req, res) => {
+    const id = req.params.id;
+    const comments = await commentsDao.findAllCommentsByAuthorId(id);
+}
+
+export default CommentsController;
