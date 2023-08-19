57a56f5167d9ab38d5261116f634f0b779e6e25f
Jinhyun Hwang <jinhyunh0808@gmail.com>
2023-08-19 16:57:08 -0400

    Added proper mongodb database link

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import session from "express-session";
 import "dotenv/config";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb+srv://wdev59842:<password>@cluster0.uh2oh9q.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_STRING = 'mongodb+srv://wdev59842:supersecretpassword@cluster0.uh2oh9q.mongodb.net/youboxd?retryWrites=true&w=majority'
 console.log(CONNECTION_STRING)
 mongoose.connect(CONNECTION_STRING);
 
