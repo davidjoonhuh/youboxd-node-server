ab331e82f481276c7b1b1a9b4c53ec4e46493fea
jwzhou1 <zhou.jiaw@northeastern.edu>
2023-08-20 14:37:43 +0800

    profile page backend.

--- users/users-controller.js
+++ users/users-controller.js
@@ -1,58 +1,68 @@
+import * as userDao from "./users-dao.js"
 import * as usersDao from "./users-dao.js";
-
 const UserController = (app) => {
-   app.get('/api/users', findAllUsers)
-   app.get('/api/users/:id', findUserById);
-   app.post('/api/users', createUser);
-   app.delete('/api/users/:id', deleteUser);
-   app.put('/api/users/:id', updateUser);
-}
-const findAllUsers = async (req, res) => {
-   const username = req.query.username;
-   const password = req.query.password;
-   if (username && password) {
-      const user = await usersDao.findUserByCredentials(username, password);
-      if (user) {
-         res.json(user);
+   const findAllUsers = async(req, res) => {
+      const username = req.query.username;
+      const password = req.query.password;
+      if (username && password) {
+         const user = await userDao.findUserByCredentials(username,password)
+         if (user) {
+            res.json(user);
+         } else {
+            res.sendStatus(404);
+         }
+      } else if (username) {
+         const user = await userDao.findUserByUsername(username)
+         if (user) {
+            res.json(user);
+         } else {
+            res.sendStatus(404);
+         }
       } else {
-         res.sendStatus(404);
+         console.log("yes")
+         const users = await userDao.findAllUsers()
+         res.json(users)
       }
-   } else if (username) {
-      const user = await usersDao.findUserByUsername(username);
-      if (user) {
-         res.json(user);
-      } else {
-         res.sendStatus(404);
-      }
-   } else {
-      const users = await usersDao.findAllUsers();
-      res.json(users);
+   };
+
+   const findUserById = async(req, res) => {
+      const userId = req.params.uid;
+      const user = await userDao.findUserById(userId)
+      res.json(user)
    }
-};
 
-const findUserById = async (req, res) => {
-   const id = req.params.id;
-   const user = await usersDao.findUserById(id);
-   res.json(user);
-};
+   const createUser = async(req, res) => {
+      const newUser = await userDao.createUser(req.body)
+      res.json(newUser)
+   }
 
-const createUser = async (req, res) => {
-   const newUser = await usersDao.createUser(req.body);
-   res.json(newUser);
-};
+   const deleteUser = async(req, res) => {
+      const userId = req.params['uid'];
+      const status = userDao.deleteUser(userId)
+      res.json(status)
+   }
 
-const deleteUser = async (req, res) => {
-   const id = req.params.id;
-   const status = await usersDao.deleteUser(id);
-   res.json(status);
-};
+   const updateUser = async(req, res) => {
+      const userId = req.params['uid']
+      const updates = req.body
+      const status = await userDao.updateUser(userId,updates)
+      const user = await userDao.findUserById(userId)
+      req.session["currentUser"] = user;
+      res.json(status)
+   }
 
-const updateUser = async (req, res) => {
-   const id = req.params.id;
-   const status = await usersDao.updateUser(id, req.body);
-   const user = await usersDao.findUserById(id);
-   req.session["currentUser"] = user;
-   res.json(status);
-};
+   const updatePublicUser = async (req, res) => {
+      const updates = req.body;
+      await usersDao.updateUser(updates._id, updates);
+      const user = await usersDao.findUserById(updates._id)
+      res.json(user);
+   }
 
-export default UserController
+   app.get("/api/users", findAllUsers)
+   app.get("/api/users/:uid", findUserById)
+   app.post("/api/users", createUser)
+   app.delete("/api/users/:uid", deleteUser)
+   app.put("/api/users/:uid", updateUser)
+   app.put('/api/publicuser', updatePublicUser);
+}
+export default UserController;
--- users/users-schema.js
+++ users/users-schema.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
 const usersSchema = new mongoose.Schema({
+  _id:Number,
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  firstName: String,
-  lastName: String,
+  firstName: { type: String, default: "None" },
+  lastName: { type: String, default: "None" },
+  email: { type: String, default: "None" },
+  phone: { type: String, default: "000-000-0000" },
+  role: {
+    type: String,
+    default: "Viewer",
+  },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'usersModel' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'usersModel' }],
 }, { collection: "users" });
 export default usersSchema;
