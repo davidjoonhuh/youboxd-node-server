f02897aa2003a65b18dfcdac7350118a45f2cdac
jwzhou1 <115595414+jwzhou1@users.noreply.github.com>
2023-08-21 21:16:38 +0800

    Update users-schema.js

--- users/users-schema.js
+++ users/users-schema.js
@@ -1,8 +1,18 @@
 import mongoose from "mongoose";
 const usersSchema = new mongoose.Schema({
+  _id:Number,
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  firstName: String,
-  lastName: String,
+  firstName: { type: String, default: "None" },
+  lastName: { type: String, default: "None" },
+  email: { type: String, default: "None" },
+  phone: { type: String, default: "000-000-0000" },
+  address: { type: String, default: "None" },
+  role: {
+    type: String,
+    default: "Viewer",
+  },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
 }, { collection: "users" });
 export default usersSchema;
