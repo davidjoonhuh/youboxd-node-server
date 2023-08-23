d3b9e154f80db59b9a22c38edbd8ab30bbca6c03
jwzhou1 <115595414+jwzhou1@users.noreply.github.com>
2023-08-24 06:31:52 +0800

    add username color

--- users/users-schema.js
+++ users/users-schema.js
@@ -7,6 +7,7 @@ const usersSchema = new mongoose.Schema({
   email: { type: String, default: "None" },
   phone: { type: String, default: "000-000-0000" },
   address: { type: String, default: "None" },
+  color: { type: String, default: "black" },
   role: {
     type: String,
     default: "Viewer",
