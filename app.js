374c6b110d7b800d3d41d66feb89dd8728bb068c
jwzhou1 <115595414+jwzhou1@users.noreply.github.com>
2023-08-24 22:33:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,35 +8,73 @@ import session from "express-session";
 import "dotenv/config";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = 'mongodb+srv://wdev59842:supersecretpassword@cluster0.uh2oh9q.mongodb.net/youboxd?retryWrites=true&w=majority'
-console.log(CONNECTION_STRING)
-mongoose.connect(CONNECTION_STRING);
 
 
+mongoose.connect("mongodb+srv://wdev59842:supersecretpassword@cluster0.uh2oh9q.mongodb.net/youboxd?retryWrites=true&w=majority")
 const app = express();
+
+
+app.use((req, res, next) => {
+  const allowedOrigins = ["http://localhost:3000"];
+  const incomingOrigin = req.headers.origin;
+
+  if (allowedOrigins.includes(incomingOrigin)) {
+    res.header("Access-Control-Allow-Origin", incomingOrigin);
+  }
+
+  res.header("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+      "GET, PUT, POST, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  next();
+});
+
+app.use(express.json());
 app.use(
-    cors({
-        credentials: true,
-        origin: process.env.FRONTEND_URL
+    session({
+      secret: "any string",
+      resave: false,
+      saveUninitialized: true,
     })
 );
-const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-    };
-  }
-  app.use(session(sessionOptions));
-  
 app.use(express.json());
 AuthController(app);
 UserController(app);
 CommentsController(app);
 VideosController(app);
 app.listen(process.env.PORT || 4000);
+
+// const CONNECTION_STRING = 'mongodb+srv://wdev59842:supersecretpassword@cluster0.uh2oh9q.mongodb.net/youboxd?retryWrites=true&w=majority'
+// console.log(CONNECTION_STRING)
+// mongoose.connect(CONNECTION_STRING);
+
+
+// const app = express();
+// app.use(
+//     cors({
+//         credentials: true,
+//         origin: process.env.FRONTEND_URL
+//     })
+// );
+// const sessionOptions = {
+//     secret: "any string",
+//     resave: false,
+//     saveUninitialized: false,
+// };
+// if (process.env.NODE_ENV !== "development") {
+//     sessionOptions.proxy = true;
+//     sessionOptions.cookie = {
+//       sameSite: "none",
+//       secure: true,
+//     };
+//   }
+//   app.use(session(sessionOptions));
+  
+// app.use(express.json());
+// AuthController(app);
+// UserController(app);
+// CommentsController(app);
+// VideosController(app);
+// app.listen(process.env.PORT || 4000);
